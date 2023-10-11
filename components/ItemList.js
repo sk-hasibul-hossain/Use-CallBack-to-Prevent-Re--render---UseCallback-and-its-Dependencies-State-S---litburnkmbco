970d23f9d703070464c6ef5ed69b76105d182e0a
Hasibul <skhasibulhossain16@gmail.com>
2023-10-11 22:45:34 +0530

    ready for test

--- components/ItemList.js
+++ components/ItemList.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 
 function ItemList({ items }) {
-    console.log("ItemList rendered!"); // DO NOT EDIT THIS LINE
+  console.log("ItemList rendered!"); // DO NOT EDIT THIS LINE
 
-    return (
-        <ul>
-            {items.map((item, idx) => <li key={idx}>{item}</li>)}
-        </ul>
-    );
+  return (
+    <ul>
+      {items.map((item, idx) => (
+        <li key={idx}>{`${item} ${idx + 1}`}</li>
+      ))}
+    </ul>
+  );
 }
 
 export default React.memo(ItemList);
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,26 @@
-import { useState, useCallback } from 'react';
-import ItemList from '../components/ItemList';
+import { useState, useCallback, useEffect, useRef } from "react";
+import ItemList from "../components/ItemList";
 
 export default function Home() {
-    const [items, setItems] = useState([]);
-    const [message, setMessage] = useState('');
+  const [items, setItems] = useState([]);
+  const [message, setMessage] = useState("");
+  const ref = useRef();
+  const optimize = useCallback(() => {
+    setItems((oldValue) => {
+      return [...oldValue, "Item"];
+    });
+    setMessage("Item added syccessfully!");
+    clearTimeout(ref.current);
+    ref.current = setTimeout(() => {
+      setMessage("");
+    }, 3000);
+  }, [setItems]);
 
-    
-
-    return (
-        <div>
-            <button >Add Item</button>
-            <ItemList items={items} />
-            {message && <p id='message'>{message}</p>}
-        </div>
-    );
+  return (
+    <div>
+      <button onClick={optimize}>Add Item</button>
+      <ItemList items={items} />
+      {message && <p id="message">{message}</p>}
+    </div>
+  );
 }
