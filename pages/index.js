4ed39dac67be444e6a142428f7bc3a21e0bf2d2e
Hasibul <skhasibulhossain16@gmail.com>
2023-10-13 21:10:56 +0530

    add code

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,12 @@ import ItemList from "../components/ItemList";
 export default function Home() {
   const [items, setItems] = useState([]);
   const [message, setMessage] = useState("");
-  const ref = useRef();
   const optimize = useCallback(() => {
     setItems((oldValue) => {
       return [...oldValue, "Item"];
     });
     setMessage("Item added syccessfully!");
-    clearTimeout(ref.current);
-    ref.current = setTimeout(() => {
+    setTimeout(() => {
       setMessage("");
     }, 3000);
   }, [setItems]);
